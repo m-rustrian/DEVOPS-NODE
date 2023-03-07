ad8465654b09ecad0c13ce5d0967e6b85356d5d6
Miguel Rustrian <mrustrian@globalbithouse.com>
2023-03-07 23:15:24 +0100

    add public html

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
- res.status(200).json({
-    message: 'Hola mundo'
- })
-})
+    res.sendFile(__dirname + '/public/index.html');
+  });
+  
+  app.get('/section', (req, res) => {
+    res.sendFile(__dirname + '/public/section.html');
+  });
+
+// app.get('/', (req, res) => {
+//  res.status(200).json({
+//     message: 'Hola mundo'
+//  })
+// })
+
+
 
 app.listen(port, () => {
  console.log(`Servidor escuchando en http://localhost:${port}`);
