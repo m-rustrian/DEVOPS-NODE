dcb1553e6882eed5e51e25c3d678f7c8f76c9e7a
Miguel Rustrian <mrustrian@globalbithouse.com>
2023-03-07 18:59:30 +0100

    script and port to heroku

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
  res.status(200).json({
- message: 'Hola mundo'
+    message: 'Hola mundo'
  })
 })
 
